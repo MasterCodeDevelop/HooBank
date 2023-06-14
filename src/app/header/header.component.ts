a95a15268c0e955870da27dc19ceee8a97d694f7
master <master.code.develop@gmail.com>
2023-06-15 00:22:41 +0200

    feat(header): Add logo, navbar, toggle, responsiveness, and styles
    - Add logo to the header
    - Implement navbar in the header
    - Add toggle with close and menu images
    - Add responsive design for small and large screens
    - Style the header with SCSS and CSS

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  hash = window.location.hash;
+  toggle = false;
 
+  setActive = (e: any) => {
+    const { classList } = e.target,
+      active = e.target.parentElement.parentElement.querySelector('.active');
+    if (active) active.classList.remove('active');
+    if (!classList.contains('active')) classList.add('active');
+    this.toggle = false;
+  };
+
+  setToggle = () => {
+    this.toggle = !this.toggle;
+  };
+  navLinks = [
+    {
+      id: 'home',
+      title: 'Home',
+    },
+    {
+      id: 'features',
+      title: 'Features',
+    },
+    {
+      id: 'product',
+      title: 'Product',
+    },
+    {
+      id: 'clients',
+      title: 'Clients',
+    },
+  ];
 }
