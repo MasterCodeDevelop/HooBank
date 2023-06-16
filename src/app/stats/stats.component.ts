bf6721a237704bde9d88ad729ed4d5c3d3b41864
master <master.code.develop@gmail.com>
2023-06-16 12:58:11 +0200

    feat(stats): Populate 'stats' component and refactor 'text-gradient' class location
    - 'stats': Populate with data list using 'ng-for', added 'value' and 'text', implemented gradient color on text
    - Refactor: Moved 'text-gradient' class from 'home' styles to root 'styles.scss' for broader access

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.scss']
+  styleUrls: ['./stats.component.scss'],
 })
 export class StatsComponent {
-
+  stats = [
+    {
+      id: '1',
+      title: 'User Active',
+      value: '3800+',
+    },
+    {
+      id: '2',
+      title: 'Trusted by Company',
+      value: '230+',
+    },
+    {
+      id: '3',
+      title: 'Transaction',
+      value: '$230M+',
+    },
+  ];
 }
